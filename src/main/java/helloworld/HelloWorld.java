f904d928d18c0a65705f674999a0c0e3ea218e20
sivatesting0 <92081763+sivatesting0@users.noreply.github.com>
2021-11-05 17:36:35 +0530

    Update HelloWorld.java

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Jenkins welome pole scm change");
+		System.out.println("Hello Jenkins welome pole webhook");
 	}
 
 }
